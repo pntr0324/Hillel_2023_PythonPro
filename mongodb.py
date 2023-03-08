9619dbf8df4f35f18370b3f16e8c9790118dfac1
alinasmirnova0303 <alinasmirnova0303@gmail.com>
2023-03-08 17:39:32 +0200

    edit HT_9_Mongo

--- mongodb.py
+++ mongodb.py
@@ -1,5 +1,5 @@
 import pymongo
 
-client = pymongo.MongoClient('mongodb://root:example@127.0.0.1:27017')
+client = pymongo.MongoClient('mongodb://root:example@localhost:27017')
 database = client['crm_db']
 contacts_collection = database['contacts']
